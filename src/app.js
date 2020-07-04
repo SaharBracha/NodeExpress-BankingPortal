2885a1c418a58cdb4dfaddb5b68170b6b6db93e7
saharb <saharb@jfrog.com>
2020-07-04 13:11:30 +0300

    Phase 3

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const path = require('path');
+const { accounts, users, writeJSON } = require('./data.js');
+
 
 const express = require('express');
 const app = new express();
@@ -9,12 +11,6 @@ app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({extended: true}));
-
-const accountData = fs.readFileSync('src/json/accounts.json', 'utf8');
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync('src/json/users.json', 'utf8');
-const users = JSON.parse(userData);
 
 app.get('/', (req, res) => res.render('index', { title: 'Account Summary' , accounts: accounts}));
 
@@ -30,8 +26,7 @@ app.get('/payment', (req, res) => res.render('payment', {account: accounts.credi
 app.post('/payment', (req, res) => {
     accounts.credit.balance -= req.body.amount;
     accounts.credit.available += parseInt(req.body.amount);
-    let accountsJSON = JSON.stringify(accounts, null, 4)
-    fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+    writeJSON();
     res.render('payment', {message: 'Payment Successful', account: accounts.credit});
 });
 
@@ -50,8 +45,7 @@ app.get('/profile', (req, res) => {
 app.post('/transfer', (req, res) => {
     accounts[req.body.from].balance -= req.body.amount;
     accounts[req.body.to].balance += parseInt(req.body.amount, 10);
-    let accountsJSON = JSON.stringify(accounts, null, 4)
-    fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+    writeJSON();
     res.render('transfer', {message: 'Transfer Completed'});
 });
 
